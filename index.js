58ea5dc377c5c9b414151804b76bd4f33769a750
Donna Amos <donnacamos.developer@gmail.com>
2019-09-16 03:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function BoardMember(name, homeState, training){
-  this.name = name
-  this.homeState = homeState
-  this.training = training
+function BoardMember (name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
 
   this.veto = function(){
-    console.log("No, I must disagree")
+    return "No, I must disagree"
   }
 
-  this.approve = function(){
-    console.log("You can do that!")
+  this.approve = function() {
+    return "You can do that!"
   }
 
-  this.doCharity = function(){
-    console.log("I like to help people.")
+  this.doCharity = function() {
+    return "I like to help people."
   }
 
-  this.releasePressStatement = function(){
-    console.log("You will see great things from Scuber.")
+  this.releasePressStatement = function() {
+    return "You will see great things from Scuber."
   }
 
-  this.sayHi = function(){
-    console.log(`Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`)
+  this.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
